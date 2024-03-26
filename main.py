9cf1bcab77fc594ec919166df2e0740062173256
melvnl <melvinlim2000@gmail.com>
2024-03-27 00:58:14 +0700

    feat: update main function

--- main.py
+++ main.py
@@ -1,49 +1,58 @@
 import csv
+import sys
+import time
 from utils import time_decorator
 
-@time_decorator
-def calculate_stats(csv_filename):
-    station_temperatures = {}
-    station_counts = {}
-    flag = 1
+class StationData:
+    def __init__(self, name, min_temp, max_temp, sum_temp, count):
+        self.name = name
+        self.min = min_temp
+        self.max = max_temp
+        self.sum = sum_temp
+        self.count = count
 
-    with open(csv_filename, 'r', newline='') as csvfile:
+@time_decorator
+def run():
+    data = {}
+
+    with open("measurements.csv", newline='') as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
-            flag = flag+1
-            print("processing row: {}".format(flag))
-            station = row[0]
+        progress = 0
+        for idx, row in enumerate(reader):
+            name = row[0]
             temperature = float(row[1])
 
-            if station in station_temperatures:
-                station_temperatures[station].append(temperature)
+            if name not in data:
+                data[name] = StationData(name, temperature, temperature, temperature, 1)
             else:
-                station_temperatures[station] = [temperature]
+                station = data[name]
+                if temperature < station.min:
+                    station.min = temperature
+                if temperature > station.max:
+                    station.max = temperature
+                station.sum += temperature
+                station.count += 1
 
-            if station in station_counts:
-                station_counts[station] += 1
-            else:
-                station_counts[station] = 1
+            if (idx + 1) * 100 // 1000000000 != progress:
+                progress = (idx + 1) * 100 // 1000000000
+                bars = '=' * (progress // 2)
+                sys.stdout.write(f"\r[{bars:<50}] {progress}%")
+                sys.stdout.flush()
+    sys.stdout.write('\n')
 
-    station_stats = {}
-    for station, temperatures in sorted(station_temperatures.items()):
-        min_temp = min(temperatures)
-        mean_temp = sum(temperatures) / station_counts[station]
-        max_temp = max(temperatures)
-        station_stats[station] = (min_temp, mean_temp, max_temp)
+    print_result(data)
 
-    return station_stats
+def print_result(data):
+    sorted_keys = sorted(data.keys())
+    print("{", end="")
+    for key in sorted_keys:
+        station = data[key]
+        print(f"{station.name}={station.min:.1f}/{station.sum/station.count:.1f}/{station.max:.1f}, ", end="")
+    print("}")
 
-def print_station_stats(station_stats):
-    result = '{'
-    for station, stats in sorted(station_stats.items()):
-        min_temp, mean_temp, max_temp = stats
-        result += '{}={:.1f}/{:.1f}/{:.1f}, '.format(station, min_temp, mean_temp, max_temp)
-    result = result.rstrip(', ') + '}'
-    print(result)
+def main():
+    # INFO: run took 6 minutes 22 seconds to execute.
+    run()
 
-# Calculate statistics
-station_stats = calculate_stats('measurements.csv')
-
-# Print statistics
-print_station_stats(station_stats)
+if __name__ == "__main__":
+    main()
