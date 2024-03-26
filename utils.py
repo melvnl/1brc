b9c7cec0b78db90db90f386cdb229e7261a1e802
melvnl <melvinlim2000@gmail.com>
2024-03-26 23:26:08 +0700

    feat: upgrade time_decorator log information UX

--- utils.py
+++ utils.py
@@ -5,8 +5,21 @@ def time_decorator(func):
         start_time = timer()
         result = func(*args, **kwargs)
         end_time = timer()
-        elapsed_time = end_time - start_time
-        print("INFO: {} took {} seconds to execute.".format(func.__name__, round(elapsed_time, 2)))
+        seconds = end_time - start_time
+
+        if seconds < 60:
+            print("INFO: {} took {} seconds to execute.".format(func.__name__, round(seconds, 2)))
+        elif seconds < 3600:
+            minutes, seconds = divmod(seconds, 60)
+            print("INFO: {} took {} minutes {} seconds to execute.".format(func.__name__, int(minutes), int(seconds)))
+        else:
+            hours, remainder = divmod(seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            if minutes == 0:
+                print("INFO: {} took {} hours {} seconds to execute.".format(func.__name__, int(hours), int(seconds)))
+            else:
+                print("INFO: {} took {} hours {} minutes {} seconds to execute.".format(func.__name__, int(hours), int(minutes), int(seconds)))
+
         return result
 
     return wrapper
